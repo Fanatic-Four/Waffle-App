9d657441a6c31b06021e91f9182239c41aa080b4
Kayla Nguyen <nguye27k@mtholyoke.edu>
2016-03-26 00:34:09 -0400

    Add register page and action, add ShowEvent page

---
+++ Waffle/app/src/main/java/fanaticfour/waffle/ShowEvent.java
@@ -0,0 +1,13 @@
+package fanaticfour.waffle;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class ShowEvent extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_event);
+    }
+}
--- Waffle/app/src/main/java/fanaticfour/waffle/register.java
+++ Waffle/app/src/main/java/fanaticfour/waffle/register.java
@@ -1,14 +1,58 @@
 package fanaticfour.waffle;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class register extends Activity {
+
+    // UI references.
+    private AutoCompleteTextView mUsernameView;
+    private EditText mPasswordView;
+    private EditText mPhoneNumView;
+    private View mProgressView;
+    private View mRegisterFormView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        // Set up the register form.
+        mUsernameView = (AutoCompleteTextView) findViewById(R.id.username);
+        //populateAutoComplete();
+
+        mPasswordView = (EditText) findViewById(R.id.password);
+
+        mPhoneNumView = (EditText) findViewById(R.id.phonenum);
+
+        Button mRegisterButton = (Button) findViewById(R.id.register_button);
+        mRegisterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                register(view);
+            }
+        });
+
+        mRegisterFormView = findViewById(R.id.register_form);
+        mProgressView = findViewById(R.id.register_progress);
     }
+
+    private void register(View v){
+        Intent intent = new Intent(this, ShowEvent.class);
+        startActivity(intent);
+    }
+
+//    private void populateAutoComplete() {
+//        getLoaderManager().initLoader(0, null, this);
+//    }
+
 
 }
