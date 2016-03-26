93cd788e37ff9cf5c94fb08667f0c4fac118093e
Kayla Nguyen <nguye27k@mtholyoke.edu>
2016-03-26 03:12:49 -0400

    add create event functionality, connect pages together

--- Waffle/app/src/main/java/fanaticfour/waffle/CreateEvent.java
+++ Waffle/app/src/main/java/fanaticfour/waffle/CreateEvent.java
@@ -1,13 +1,95 @@
 package fanaticfour.waffle;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.annotation.TargetApi;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.view.View;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
 
 public class CreateEvent extends Activity {
+
+    // UI references.
+    private AutoCompleteTextView mUsernameView;
+    private EditText eventName;
+
+    private View mProgressView;
+    private View mCreateFormView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_event);
+
+        // Set up the register form.
+        eventName = (EditText) findViewById(R.id.event_name);
+
+        Button createEventButton = (Button) findViewById(R.id.create_event_button);
+        createEventButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                createEvent(view);
+            }
+        });
+
+        mCreateFormView = findViewById(R.id.create_event_form);
+        mProgressView = findViewById(R.id.create_event_progress);
+    }
+
+    private void createEvent(View v) {
+        showProgress(true);
+        Intent intent = new Intent(this, ShowEvent.class);
+        startActivity(intent);
+    }
+
+    /**
+     * Shows the progress UI and hides the login form.
+     */
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
+    public void showProgress(final boolean show) {
+        // On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
+        // for very easy animations. If available, use these APIs to fade-in
+        // the progress spinner.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
+            int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
+
+            mCreateFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+            mCreateFormView.animate().setDuration(shortAnimTime).alpha(
+                    show ? 0 : 1).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    mCreateFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+                }
+            });
+
+            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+            mProgressView.animate().setDuration(shortAnimTime).alpha(
+                    show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+                }
+            });
+        } else {
+            // The ViewPropertyAnimator APIs are not available, so simply show
+            // and hide the relevant UI components.
+            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+            mCreateFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+        }
     }
 }
