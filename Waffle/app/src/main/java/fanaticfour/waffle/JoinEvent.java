2037046cb8f608d9672a0b888e7203d7206da24b
Kayla Nguyen <nguye27k@mtholyoke.edu>
2016-03-26 03:50:46 -0400

    create join event page and link to action

--- Waffle/app/src/main/java/fanaticfour/waffle/JoinEvent.java
+++ Waffle/app/src/main/java/fanaticfour/waffle/JoinEvent.java
@@ -1,13 +1,93 @@
 package fanaticfour.waffle;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.annotation.TargetApi;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.view.View;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
 
 public class JoinEvent extends Activity {
+
+    // UI references.
+    private View mProgressView;
+    private View mJoinFormView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_join_event);
+        setContentView(R.layout.activity_register);
+
+        Button mJoinButton = (Button) findViewById(R.id.join_button);
+        mJoinButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                join(view);
+            }
+        });
+
+        mJoinFormView = findViewById(R.id.join_form);
+        mProgressView = findViewById(R.id.join_progress);
     }
+
+    private void join(View v) {
+        showProgress(true);
+        String ticketNum = ((EditText)findViewById(R.id.ticketNumber)).getText().toString();
+
+        Intent intent = new Intent(this, ShowEvent.class);
+        startActivity(intent);
+    }
+
+    /**
+     * Shows the progress UI and hides the login form.
+     */
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
+    public void showProgress(final boolean show) {
+        // On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
+        // for very easy animations. If available, use these APIs to fade-in
+        // the progress spinner.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
+            int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
+
+            mJoinFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+            mJoinFormView.animate().setDuration(shortAnimTime).alpha(
+                    show ? 0 : 1).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    mJoinFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+                }
+            });
+
+            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+            mProgressView.animate().setDuration(shortAnimTime).alpha(
+                    show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+                }
+            });
+        } else {
+            // The ViewPropertyAnimator APIs are not available, so simply show
+            // and hide the relevant UI components.
+            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+            mJoinFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+        }
+    }
+
+
 }
--- Waffle/app/src/main/java/fanaticfour/waffle/ShowEvent.java
+++ Waffle/app/src/main/java/fanaticfour/waffle/ShowEvent.java
@@ -92,7 +92,7 @@ public class ShowEvent extends Activity {
             }
         });
 
-        populateEventsList();
+//        populateEventsList();
     }
 
     private void populateEventsList() {
